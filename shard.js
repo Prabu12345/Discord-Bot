99875d874660a598e924563fa445b658d5837b24
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2020-08-06 13:30:53 +0700

    new

--- shard.js
+++ shard.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js')
 const manager = new ShardingManager('./index.js', {
-  token: process.env.token,
+  token: process.env.DISCORD_TOKEN,
   totalShards: parseInt(process.env.SHARD_COUNT) || 'auto'
 })
 
