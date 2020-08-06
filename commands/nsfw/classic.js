b8329c463488ff8d1340d4d19df343b066f292d7
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2020-08-06 09:52:29 +0700

    Update classic.js

--- commands/nsfw/classic.js
+++ commands/nsfw/classic.js
@@ -8,15 +8,13 @@ description: "",
 run: async (client, message, args, level) => {
 //command
 if (!message.channel.nsfw) return message.channel.send('You can use this command in an NSFW Channel!')
-superagent.get('https://nekos.life/api/v2/img/classic')
-    .end((err, response) => {
+const { body: { url } } = await superagent.get('https://nekos.life/api/v2/img/classic')
   const lewdembed = new Discord.MessageEmbed()
   .setTitle("Classic")
-  .setImage(response.body.url)
+  .setImage(url)
   .setColor(`#000000`)
   .setFooter(`Tags: Classic`)
-  .setURL(response.body.url);
 message.channel.send(lewdembed);
-})
+
 }
 };
