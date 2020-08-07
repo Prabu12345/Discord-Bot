93afbefb3780e0fefd5a2d033aca0bec8dd5c349
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2020-08-07 10:10:14 +0700

    a

--- auth.js
+++
@@ -1,1 +0,0 @@
-exports.release = process.env.token;
---
+++ src/commands/anime/neko.js
@@ -0,0 +1,28 @@
+const { Command, SwitchbladeEmbed } = require('../../')
+const fetch = require('node-fetch')
+const nekoAPI = 'https://nekos.life/api/v2/img/'
+
+module.exports = class Neko extends Command {
+  constructor (client) {
+    super({
+      name: 'neko',
+      aliases: ['nekogirl'],
+      category: 'anime'
+    }, client)
+  }
+
+  async run ({ t, author, channel }) {
+    const embed = new SwitchbladeEmbed(author)
+    channel.startTyping()
+
+    // Send a lewd neko if the channel is NSFW
+    const endpoint = channel.nsfw ? 'lewd' : 'neko'
+
+    const { url } = await fetch(nekoAPI + endpoint).then(res => res.json())
+
+    embed.setImage(url)
+      .setDescription(t('commands:neko.hereIsYour', { context: endpoint }))
+
+    channel.send(embed).then(() => channel.stopTyping())
+  }
+}
