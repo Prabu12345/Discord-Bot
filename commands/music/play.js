519ccb590ced4d92958e16b5a20cd6521195c0df
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2020-12-28 19:43:25 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -179,7 +179,7 @@ module.exports = class PlayCommand extends Command {
       return;
     }
         youtube
-          .getVideoByID(videos.id)
+          .getVideo(videos.url)
           .then(video => {
             // // can be uncommented if you don't want the bot to play live streams
             // if (video.raw.snippet.liveBroadcastContent === 'live') {
