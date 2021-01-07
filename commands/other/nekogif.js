ba2b078b72b9a3b173416737a9980cf78cb84bb5
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-01-07 13:15:19 +0700

    Create nekogif.js

---
+++ commands/other/nekogif.js
@@ -0,0 +1,41 @@
+const { Command } = require('discord.js-commando');
+const { normalcolor, errorcolor } = require('../../config.json')
+const { MessageEmbed } = require('discord.js');
+const nekos = require('nekos.life');
+
+module.exports = class CatCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'nekogif',
+      aliases: ['neko-gif'],
+      group: 'anime',
+      memberName: 'nekogif',
+      description: 'Showing a Neko picture, if you want a lewd turn on nsfw',
+      throttling: {
+        usages: 2,
+        duration: 10
+      }
+    });
+  }
+
+  async run(message) {
+    const neko = new nekos();
+    let neks = await neko.sfw.nekoGif();
+    let nekh = await neko.nsfw.nekoGif();
+    if (!message.channel.nsfw) {
+    let embed = new MessageEmbed()
+    .setTitle('Neko Gif')
+    .setImage(neks.url)
+    .setColor(normalcolor);
+    message.channel.send(embed);
+    } else {
+    let embed = new MessageEmbed()
+    .setTitle('Lewd Neko Gif')
+    .setImage(nekh.url)
+    .setColor(normalcolor);
+    message.channel.send(embed);
+    }
+  } catch (err) {
+    message.channel.send('There was an error!\n' + err).catch();
+  }
+};
