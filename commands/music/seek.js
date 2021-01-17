a19d5b8bb9ff383377b5b5984b595515429d01b2
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-01-17 17:40:38 +0700

    Update Queue, Added seek ( Test ) Command

---
+++ commands/music/seek.js
@@ -0,0 +1,55 @@
+const { Command } = require('discord.js-commando');
+const { normalcolor, errorcolor } = require('../../config.json')
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class LoopCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'seek',
+      group: 'music',
+      memberName: 'seek',
+      guildOnly: true,
+      description: 'Loop the current playing song',
+      examples: ['seek 1:30', 'seek 0:30'],
+      args: [
+        {
+          key: 'time',
+          type: 'time',
+          prompt: 'Enter seek time. E.g. 1:30 or 0:30'
+        }
+      ]
+    });
+  }
+
+  run(message, queue, { time }) {
+    if (!message.guild.musicData.isPlaying) {
+      const errloopEmbed = new MessageEmbed()
+      .setColor(errorcolor)
+      .setDescription('There is no song playing right now!')
+      return message.say(errloopEmbed);
+    } else if (
+      message.guild.musicData.isPlaying &&
+      message.guild.triviaData.isTriviaRunning
+    ) {
+      const errloopEmbed = new MessageEmbed()
+      .setColor(errorcolor)
+      .setDescription('You cannot seek over a trivia!')
+      return message.say(errloopEmbed);
+    } else if (
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    ) {
+      const errloopEmbed = new MessageEmbed()
+      .setColor(errorcolor)
+      .setDescription(`You must be in the same voice channel as the bot's in order to use that!`)
+      message.reply(errloopEmbed);
+      return;
+    }
+    const video = message.guild.musicData.nowPlaying;
+    const loopEmbed = new MessageEmbed()
+    .setColor(normalcolor)
+      loopEmbed.setDescription('Looped **One track**, **loop off** if you want to stop looping!')
+      message.say(loopEmbed)
+      queue.connection.play(video.url, {seek: time.total.seconds / 1000})
+    return;
+  }
+};
