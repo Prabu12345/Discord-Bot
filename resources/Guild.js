b6d6d38c25d3c1441f5be39919506dbc460076ae
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-01-20 14:20:04 +0700

    Update Guild.js

--- resources/Guild.js
+++ resources/Guild.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
- const guildSchema = monggoose.Schema({
+ const guildSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.Objectid,
     guildID: String,
     guildName: String,
     prefix: String
  })
 
- module.exports = monggoose.model('Guild', guildSchema, 'guild');
+ module.exports = mongoose.model('Guild', guildSchema, 'guild');
