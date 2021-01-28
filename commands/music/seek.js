b9696c5ad27a59a1a317b56cb00f47793715f1ad
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-01-28 14:56:24 +0700

    Update seek.js

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { normalcolor, errorcolor } = require('../../config.json')
 const { MessageEmbed } = require('discord.js');
-const ytdl = require('ytdl-core');
+const ytdl = require('discord-ytdl-core');
 const { playSong } = require('./play')
 
 module.exports = class LoopCommand extends Command {
@@ -55,15 +55,19 @@ module.exports = class LoopCommand extends Command {
     .setDescription('seek')
     message.say(loopEmbed)
     message.guild.musicData.songDispatcher.end();
+    message.guild.musicData.isPlaying = true;
 
     seekplaying.voiceChannel.join()
     .then(connection => {
       const dispatcher = connection
           .play(
             ytdl(seekplaying.url, {
+              filter: "audioonly",
+              opusEncoded: true,
+              bitrate: 320,
+              seek: time / 1000,
               quality: 'highestaudio',
               highWaterMark: 1 << 25,
-              begin: time
             })
           ) 
           .on('start', function() {
