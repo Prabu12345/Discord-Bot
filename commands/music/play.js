f872f6adc5c3bc52bac098d5c5449a48a52ea911
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-02-25 18:39:56 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -148,7 +148,7 @@ module.exports = class PlayCommand extends Command {
       const tracks = []
       var nameOfplaylist = playlist.name
       for (let i = 0; i < playlist.tracks.items.length; i++) {
-        const updatequery = `${playlist.tracks.items[i].track.artists.name} - ${playlist.tracks.items[i].track.name}`
+        const updatequery = `${playlist.tracks.items[i].track.artists[0].name} - ${playlist.tracks.items[i].track.name}`
         const results = await youtube.searchVideos(updatequery, 1).catch(async function() {
           const errvideoEmbed = new MessageEmbed()
           .setColor(errorcolor)
