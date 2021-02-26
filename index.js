7d59a21acd8240ca17baa0e2c5d8a2929b83942f
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-02-26 22:01:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ Structures.extend('Guild', function(Guild) {
 const client = new CommandoClient({
   commandPrefix: prefix,
   owner: discord_owner_id, // value comes from config.json
-  partials: ['MESSAGE', 'REACTION']
 });
 
 fs.readdir('./resources/event/', (err, files) => {
@@ -124,7 +123,39 @@ client.on('voiceStateUpdate', async (___, newState) => {
       stop()
     }
   } 
-});
+})
+const MessageModel = require('../Guild');
+client.on("messageReactionAdd", (client, reaction, user) => {
+  let addMemberRole = (emojiRoleMappings) => {
+    if(emojiRoleMappings.hasOwnProperty(reaction.emoji.id)) {
+        let roleId = emojiRoleMappings[reaction.emoji.id];
+        let role = reaction.message.guild.roles.cache.get(roleId);
+        let member = reaction.message.guild.members.cache.get(user.id);
+        if(role && member) {
+            member.roles.add(role);
+        }
+    }
+  }
+  if(reaction.message.partial) {
+    await reaction.message.fetch();
+    let { id } = reaction.message;
+    try {
+        let msgDocument = await MessageModel.findOne({ messageId: id });
+        if(msgDocument) {
+            client.cachedMessageReactions.set(id, msgDocument.emojiRoleMappings);
+            let { emojiRoleMappings } = msgDocument;
+            addMemberRole(emojiRoleMappings);
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+  }
+  else {
+    let emojiRoleMappings = client.cachedMessageReactions.get(reaction.message.id);
+    addMemberRole(emojiRoleMappings);
+  }
+})
 
 client.login(process.env.token);
 
