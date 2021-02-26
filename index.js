1fbc420147a266bc1acef59ec69ac27062d4f7a2
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-02-26 22:21:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -124,37 +124,40 @@ client.on('voiceStateUpdate', async (___, newState) => {
     }
   } 
 })
+
 const MessageModel = require('../Guild');
-client.on("messageReactionAdd", (client, reaction, user) => {
-  let addMemberRole = (emojiRoleMappings) => {
-    if(emojiRoleMappings.hasOwnProperty(reaction.emoji.id)) {
-        let roleId = emojiRoleMappings[reaction.emoji.id];
-        let role = reaction.message.guild.roles.cache.get(roleId);
-        let member = reaction.message.guild.members.cache.get(user.id);
-        if(role && member) {
-            member.roles.add(role);
-        }
+client.on("messageReactionAdd", (reaction, user) => {
+  (async () => {
+    let addMemberRole = (emojiRoleMappings) => {
+      if(emojiRoleMappings.hasOwnProperty(reaction.emoji.id)) {
+          let roleId = emojiRoleMappings[reaction.emoji.id];
+          let role = reaction.message.guild.roles.cache.get(roleId);
+          let member = reaction.message.guild.members.cache.get(user.id);
+          if(role && member) {
+              member.roles.add(role);
+          }
+      }
     }
-  }
-  if(reaction.message.partial) {
-    await reaction.message.fetch();
-    let { id } = reaction.message;
-    try {
-        let msgDocument = await MessageModel.findOne({ messageId: id });
-        if(msgDocument) {
-            client.cachedMessageReactions.set(id, msgDocument.emojiRoleMappings);
-            let { emojiRoleMappings } = msgDocument;
-            addMemberRole(emojiRoleMappings);
-        }
+    if(reaction.message.partial) {
+      await reaction.message.fetch();
+      let { id } = reaction.message;
+      try {
+          let msgDocument = await MessageModel.findOne({ messageId: id });
+          if(msgDocument) {
+              client.cachedMessageReactions.set(id, msgDocument.emojiRoleMappings);
+              let { emojiRoleMappings } = msgDocument;
+              addMemberRole(emojiRoleMappings);
+          }
+      }
+      catch(err) {
+          console.log(err);
+      }
     }
-    catch(err) {
-        console.log(err);
+    else {
+      let emojiRoleMappings = client.cachedMessageReactions.get(reaction.message.id);
+      addMemberRole(emojiRoleMappings);
     }
-  }
-  else {
-    let emojiRoleMappings = client.cachedMessageReactions.get(reaction.message.id);
-    addMemberRole(emojiRoleMappings);
-  }
+  })();
 })
 
 client.login(process.env.token);
