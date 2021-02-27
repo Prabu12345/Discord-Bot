8e07dd5f6125b14ba138f839cbbe458d7a9e831c
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-02-27 20:39:17 +0700

    Create play-playlist.js

---
+++ commands/music/play-playlist.js
@@ -0,0 +1,49 @@
+const { Command } = require('discord.js-commando');
+const { Database } = require("quickmongo");
+const db = new Database("mongodb+srv://admin:lakilaki@cluster0.yvw90.mongodb.net/guaa?retryWrites=true&w=majority", "playlist");
+const { playSong } = require('../music/play')
+
+module.exports = class DisplayPlaylistCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'play-playlist',
+      group: 'music',
+      aliases: ['playsavesong'],
+      memberName: 'play-playlist',
+      guildOnly: true,
+      description: 'Play a saved Playlists',
+      args: [
+        {
+          key: 'playlistName',
+          prompt: 'What is the name of the playlist you would like to display?',
+          type: 'string'
+        }
+      ]
+    });
+  }
+
+  async run(message, { playlistName }) {
+    const userPlaylists = await db.get(`${message.member.id}.savedPlaylist`);
+    const found = userPlaylists.find(element => element.name == query);
+    if (found) {
+        const urlsArray = userPlaylists[userPlaylists.indexOf(found)].urls;
+        if (!urlsArray.length) {
+            message.reply(
+              `'${query}' playlist is empty, add songs to it before attempting to play it`
+            );
+            return;
+        }
+        urlsArray.map(element =>
+            message.guild.musicData.queue.push(element)
+        );
+        if (message.guild.musicData.isPlaying) {
+            message.reply(`:new: **${query}** added ${urlsArray.length} songs to the queue!`);
+        } else if (!message.guild.musicData.isPlaying) {
+            message.guild.musicData.isPlaying = true;
+            playSong(message.guild.musicData.queue, message, 0);
+        }   
+    } else {
+        message.reply(`You have no playlist named ${playlistName}`)
+    }
+  }
+};
