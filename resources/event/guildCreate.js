53d22a679cbe9ec4b5d1266797d39c5020d4b729
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-03-10 10:51:28 +0700

    Update guildCreate.js

--- resources/event/guildCreate.js
+++ resources/event/guildCreate.js
@@ -4,9 +4,8 @@ const db = new Database("mongodb+srv://admin:lakilaki@cluster0.yvw90.mongodb.net
 module.exports = async (client, guild) => {
     let new1 = await db.get(guild.id)
     if (!new1) {
-        db.createModel(guild.id)
         db.set(guild.id, { settings: [] })
-        /*db.push(`${guild.id}.settings`, { volume: 50, timeout: 60000 })*/
+        db.push(`${guild.id}.settings`, { volume: 50, timeout: 60000 })
         console.log(`I have joined to ${guild.name}`);
     } else {
     }
