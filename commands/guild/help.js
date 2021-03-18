43773c77d4901d999c6036a78f2193956a9a86c2
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-03-18 11:34:45 +0700

    Update help.js

--- commands/guild/help.js
+++ commands/guild/help.js
@@ -32,15 +32,15 @@ module.exports = class BanCommand extends Command {
 		.setColor(normalcolor)
 		.setAuthor(`${message.guild.me.displayName}`, message.member.user.avatarURL('webp', false, 16))
 		.setDescription(stripIndents`
-		**For Help Related To A Particular Command Type** -
-	  	\`${message.guild.commandPrefix}help [command name | alias] Or @${this.client.user}help [command name | alias]\`
+		**For Help Related To A Particular Command Type -**
+	  	\`${message.guild.commandPrefix}help [command name | alias] Or ${this.client.user} help [command name | alias]\`
 
-	  	**Info [2]** - \`help\`, \`whomademe\`
-		**Music [22]** - \`clear\`, \`join\`, \`leave\`, \`loop\`, \`lyrics\`, \`move\`, \`musicsettings\`, \`music-trivia\`, \`nowplaying:\`, \`pause\`, \`play\`, \`playlist\`, \`queue\`, \`remove\`, \`resume\`, \`search\`, \`seek\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop-trivia\`, \`volume\`
-	  	**Anime [3]** - \`animegif\`, \`gintama\`, \`jojo\`
-	  	**Fun [5]** - \`cat\`, \`chucknorris\`, \`fortune\`, \`insult\`, \`random\`
-	  	**Guild [4]** - \`ban\`, \`kick\`, \`prefix\`, \`prune\`
-	  	**Other [7]** - \`math\`, \`motivation\`, \`ping\`, \`reddit\`, \`remind\`, \`reminds\`, \`world-news\`
+	  	**Info [2] -** \`help\`, \`whomademe\`.
+		**Music [22] -** \`clear\`, \`join\`, \`leave\`, \`loop\`, \`lyrics\`, \`move\`, \`musicsettings\`, \`music-trivia\`, \`nowplaying:\`, \`pause\`, \`play\`, \`playlist\`, \`queue\`, \`remove\`, \`resume\`, \`search\`, \`seek\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop-trivia\`, \`volume\`.
+	  	**Anime [3] -** \`animegif\`, \`gintama\`, \`jojo\`.
+	  	**Fun [5] -** \`cat\`, \`chucknorris\`, \`fortune\`, \`insult\`, \`random\`.
+	  	**Guild [4] -** \`ban\`, \`kick\`, \`prefix\`, \`prune\`.
+	  	**Other [7] -** \`math\`, \`motivation\`, \`ping\`, \`reddit\`, \`remind\`, \`reminds\`, \`world-news\`.
 		`)
 		.setFooter(`${message.guild.me.displayName}`, this.client.user.displayAvatarURL())
 		embed.setTimestamp()
@@ -59,7 +59,7 @@ module.exports = class BanCommand extends Command {
 		  ** Description -** ${commands[0].description || "No Description provided."}
 		  ** Usage -** ${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : 'No Usage'}`)}
 		  ** Needed Permissions -** ${commands[0].guildOnly ? ' Usable only in servers' : ''}
-		  ** Aliases -** ${commands[0].aliases ? command.aliases.join(", ") : "None."}
+		  ** Aliases -** ${commands[0].aliases ? commands[0].aliases.join(", ") : "None."}
 		  `)
 		  embed.setFooter(message.guild.name, message.guild.iconURL())
 		  embed.setTimestamp()
