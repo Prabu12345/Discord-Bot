33dd7c3cf98155620cc842c3fd984dc86f4ed36e
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-03-18 11:27:20 +0700

    Update help.js

--- commands/guild/help.js
+++ commands/guild/help.js
@@ -29,7 +29,7 @@ module.exports = class BanCommand extends Command {
 
 	if (!command) {
 		const embed = new MessageEmbed()
-		.setColor(COLOR)
+		.setColor(normalcolor)
 		.setAuthor(`${message.guild.me.displayName}`, message.member.user.avatarURL('webp', false, 16))
 		.setDescription(stripIndents`
 		**For Help Related To A Particular Command Type** -
@@ -40,21 +40,21 @@ module.exports = class BanCommand extends Command {
 	  	**Anime [3]** - \`animegif\`, \`gintama\`, \`jojo\`
 	  	**Fun [5]** - \`cat\`, \`chucknorris\`, \`fortune\`, \`insult\`, \`random\`
 	  	**Guild [4]** - \`ban\`, \`kick\`, \`prefix\`, \`prune\`
-	  	**Other [7]** - \`math\`, \`motivation\`, \`prefix\`, \`reddit\`, \`remind\`, \`reminds\`, \`world-news\`
+	  	**Other [7]** - \`math\`, \`motivation\`, \`ping\`, \`reddit\`, \`remind\`, \`reminds\`, \`world-news\`
 		`)
-		.setFooter(`${message.guild.me.displayName} | Total Commands - ${client.commands.size - 1} Loaded`, client.user.displayAvatarURL())
+		.setFooter(`${message.guild.me.displayName}`, client.user.displayAvatarURL())
 		embed.setTimestamp()
 	  
 		message.channel.send(embed)
 	  } else {
 		const embed = new MessageEmbed()
-		.setColor(COLOR)
+		.setColor(normalcolor)
 		.setAuthor(oneLine`
 		**Group:** ${commands[0].group.name}
 		(\`${commands[0].groupID}:${commands[0].memberName}\`)
 		`, message.member.user.avatarURL('webp', false, 16))
 		.setTitle(`Command - ${commands[0].name}`)
-		  if (!commands) return message.channel.send(embed.setTitle("**Invalid Command!**").setDescription(`**Do \`${prefix}help\` For the List Of the Commands!**`))
+		  if (!commands) return message.channel.send(embed.setTitle("**Invalid Command!**").setDescription(`**Do \`${message.guild.commandPrefix}help\` For the List Of the Commands!**`))
 		  embed.setDescription(stripIndents`
 		  ** Description -** ${commands[0].description || "No Description provided."}
 		  ** Usage -** ${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : 'No Usage'}`)}
@@ -66,75 +66,5 @@ module.exports = class BanCommand extends Command {
 	  
 		  message.channel.send(embed)
 	  }
-        
-        if(args.command && !showAll) {
-			if(commands.length === 1) {
-                let thelp = ``
-				let help = new MessageEmbed()
-                .setColor(normalcolor)
-                .setDescription(`You must be in the same voice channel as the bot's in order to use that!`)
-                stripIndents`
-					${oneLine`
-						__Command **${commands[0].name}**:__ ${commands[0].description}
-						${commands[0].guildOnly ? ' (Usable only in servers)' : ''}
-						${commands[0].nsfw ? ' (NSFW)' : ''}
-					`}
-					**Format:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
-				`;
-				if(commands[0].aliases.length > 0) help += `\n**Aliases:** ${commands[0].aliases.join(', ')}`;
-				help += `\n${oneLine`
-					**Group:** ${commands[0].group.name}
-					(\`${commands[0].groupID}:${commands[0].memberName}\`)
-				`}`;
-				if(commands[0].details) thelp += `\n**Details:** ${commands[0].details}`;
-				if(commands[0].examples) thelp += `\n**Examples:**\n${commands[0].examples.join('\n')}`;
-
-				const messages = [];
-				try {
-					messages.push(await msg.direct(help));
-					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
-				} catch(err) {
-					messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
-				}
-				return messages;
-			} else if(commands.length > 15) {
-				return msg.reply('Multiple commands found. Please be more specific.');
-			} else if(commands.length > 1) {
-				return msg.reply(disambiguation(commands, 'commands'));
-			} else {
-				return msg.reply(
-					`Unable to identify command. Use ${msg.usage(
-						null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined
-					)} to view the list of all commands.`
-				);
-			}
-		} else {
-			const messages = [];
-			try {
-				messages.push(await msg.direct(stripIndents`
-					${oneLine`
-						To run a command in ${msg.guild ? msg.guild.name : 'any server'},
-						use ${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						For example, ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-					`}
-					To run a command in this DM, simply use ${Command.usage('command', null, null)} with no prefix.
-					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
-					Use ${this.usage('all', null, null)} to view a list of *all* commands, not just available ones.
-					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
-					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg))))
-						.map(grp => stripIndents`
-							__${grp.name}__
-							${grp.commands.filter(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg)))
-								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
-							}
-						`).join('\n\n')
-					}
-				`, { split: true }));
-				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
-			} catch(err) {
-				messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
-			}
-			return messages;
-		}
   }
 };
