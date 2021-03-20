1210f506c9cdbc1ca12039f59940cc330b1872eb
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-03-20 08:05:04 +0700

    Update help.js

--- commands/guild/help.js
+++ commands/guild/help.js
@@ -27,8 +27,6 @@ module.exports = class BanCommand extends Command {
 	const commands = this.client.registry.findCommands(command, false, message);
 	const showAll = command && command.toLowerCase() === 'all';
 
-	console.log(commands)
-
 	if (!command) {
 		const embed = new MessageEmbed()
 		.setColor(normalcolor)
@@ -88,100 +86,96 @@ module.exports = class BanCommand extends Command {
 	  } else {
 		const embed = new MessageEmbed()
 		.setColor(normalcolor)
-		.setTitle(`${commands[0].name.toUpperCase()} - ${commands[0].group.name}${commands[0].guildOnly ? ' (Usable only in servers)' : ''}`);
+		if (commands.length === 0) return message.channel.send(embed.setTitle("**Invalid Command!**").setDescription(`**Do \`${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}\` For the List Of the Commands!**`))
+		embed.setTitle(`${commands[0].name.toUpperCase()} - ${commands[0].group.name}${commands[0].guildOnly ? ' (Usable only in servers)' : ''}`);
+		if (message.channel.type !== 'dm') {
+			if (commands[0].name == 'playlist') {
+				embed.setDescription(stripIndents`
+				** | Description |** 
+				\`${commands[0].description || "No Description provided."}\`
 
-		try {
-			if (message.channel.type !== 'dm') {
-				if (commands[0].name == 'playlist') {
-					embed.setDescription(stripIndents`
-					** | Description |** 
-					\`${commands[0].description || "No Description provided."}\`
-	
-					** | Usage |** 
-					${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
-	
-					** | Detail |** 
-					\`${commands[0].details || '`No Detail provided`'}\`
-	
-					** | Examples |** 
-					\`${message.guild.commandPrefix}playlist\`
-					\`${message.guild.commandPrefix}playlist create music\`
-					\`${message.guild.commandPrefix}playlist add music https://www.youtube.com/watch?\`
-					\`${message.guild.commandPrefix}playlist play music\`
-					\`${message.guild.commandPrefix}playlist remove music 1\`
-					\`${message.guild.commandPrefix}playlist delete music\`
-					\`${message.guild.commandPrefix}playlist see\`
-	
-					** | Aliases |** 
-					\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
-					`)
-				} else {
-					embed.setDescription(stripIndents`
-					**| Description |** 
-					\`${commands[0].description || "No Description provided."}\`
-					
-					**| Usage |** 
-					${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
-					
-					**| Detail |** 
-					\`${commands[0].details || '`No Detail provided`'}\`
-					
-					**| Examples |** 
-					\`${commands[0].examples || 'No Examples provided'}\`
-					
-					**| Aliases |** 
-					\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
-					`)
-				}
-				embed.setFooter(message.guild.name, message.guild.iconURL())
-			  } else {
-				if (commands[0].name == 'playlist') {
-					embed.setDescription(stripIndents`
-					** | Description |** 
-					\`${commands[0].description || "No Description provided."}\`
-					
-					** | Usage |** 
-					${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
-					
-					** | Detail |** 
-					\`${commands[0].details || '`No Detail provided`'}\`
-					
-					** | Examples |** 
-					\`-playlist\`
-					\`-playlist create music\`
-					\`-playlist add music https://www.youtube.com/watch?\`
-					\`-playlist play music\`
-					\`-playlist remove music 1\`
-					\`-playlist delete music\`
-					\`-playlist see\`
-					
-					** | Aliases |** 
-					\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
-					`)
-				} else {
-					embed.setDescription(stripIndents`
-					**| Description |** 
-					\`${commands[0].description || "No Description provided."}\`
-					
-					**| Usage |** 
-					${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
-					
-					**| Detail |** 
-					\`${commands[0].details || '`No Detail provided`'}\`
-					
-					**| Examples |** 
-					\`${commands[0].examples || 'No Examples provided'}\`
-					
-					**| Aliases |** 
-					\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
-					`)
-				}
-			}	  
-			embed.setTimestamp()
-			return message.channel.send(embed)	  
-		} catch {
-			return message.channel.send(embed.setTitle("**Invalid Command!**").setDescription(`**Do \`${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}\` For the List Of the Commands!**`))
+				** | Usage |** 
+				${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+
+				** | Detail |** 
+				\`${commands[0].details || '`No Detail provided`'}\`
+
+				** | Examples |** 
+				\`${message.guild.commandPrefix}playlist\`
+				\`${message.guild.commandPrefix}playlist create music\`
+				\`${message.guild.commandPrefix}playlist add music https://www.youtube.com/watch?\`
+				\`${message.guild.commandPrefix}playlist play music\`
+				\`${message.guild.commandPrefix}playlist remove music 1\`
+				\`${message.guild.commandPrefix}playlist delete music\`
+				\`${message.guild.commandPrefix}playlist see\`
+
+				** | Aliases |** 
+				\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
+				`)
+			} else {
+				embed.setDescription(stripIndents`
+				**| Description |** 
+				\`${commands[0].description || "No Description provided."}\`
+				
+				**| Usage |** 
+				${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+				
+				**| Detail |** 
+				\`${commands[0].details || '`No Detail provided`'}\`
+				
+				**| Examples |** 
+				\`${commands[0].examples || 'No Examples provided'}\`
+				
+				**| Aliases |** 
+				\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
+				`)
+			}
+			embed.setFooter(message.guild.name, message.guild.iconURL())
+		  } else {
+			if (commands[0].name == 'playlist') {
+				embed.setDescription(stripIndents`
+				** | Description |** 
+				\`${commands[0].description || "No Description provided."}\`
+				
+				** | Usage |** 
+				${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+				
+				** | Detail |** 
+				\`${commands[0].details || '`No Detail provided`'}\`
+				
+				** | Examples |** 
+				\`-playlist\`
+				\`-playlist create music\`
+				\`-playlist add music https://www.youtube.com/watch?\`
+				\`-playlist play music\`
+				\`-playlist remove music 1\`
+				\`-playlist delete music\`
+				\`-playlist see\`
+				
+				** | Aliases |** 
+				\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
+				`)
+			} else {
+				embed.setDescription(stripIndents`
+				**| Description |** 
+				\`${commands[0].description || "No Description provided."}\`
+				
+				**| Usage |** 
+				${message.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+				
+				**| Detail |** 
+				\`${commands[0].details || '`No Detail provided`'}\`
+				
+				**| Examples |** 
+				\`${commands[0].examples || 'No Examples provided'}\`
+				
+				**| Aliases |** 
+				\`${commands[0].aliases.join(', ') || "No Aliases provided"}\`
+				`)
+			}
 		}	  
+		embed.setTimestamp()
+		return message.channel.send(embed)	
 	  }
   }
 };
