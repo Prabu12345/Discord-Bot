97b576f733b0e0e0f5e02b7788db9b02eced35ca
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-05-07 13:47:19 +0700

    Update join.js

--- commands/music/join.js
+++ commands/music/join.js
@@ -22,7 +22,10 @@ module.exports = class LeaveCommand extends Command {
   run(message) {
     if (!message.guild.me.hasPermission("EMBED_LINKS")) {
       return message.channel.send(`I don't have permission to send embed`);
+    } else if (!message.guild.me.hasPermission("CONNECT")) {
+      return message.channel.send(`I don't have permission to connect \`${message.guild.member.voice.channel.name}\` channel`);
     }
+
     var voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
       const errleaveEmbed = new MessageEmbed()
