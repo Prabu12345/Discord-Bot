313ac177aec7854e3fcaca6eb4131adac27d9159
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-06-16 09:35:04 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -400,12 +400,12 @@ module.exports = class PlayCommand extends Command {
       const newSongs = videosArr
       .filter((video) => video.title != "Private video" && video.title != "Deleted video")
       .map((video) => {
-        duration = PlayCommand.formatDuration(video.duration);
+        duration = 0;
         if (duration == '0:00') duration = 'Live Stream';
         return {
           url: `https://youtube.com/watch?v=${video.id}`,
           title: video.title,
-          rawDuration: PlayCommand.durationrawed(video.duration),
+          rawDuration: 0,
           duration,
           thumbnail: video.thumbnails.high.url,
           memberDisplayName: message.member.user.tag,       
