80247df6e1c4a4a9eca8feabba71d732f2f8423a
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-08-31 11:23:03 +0700

    aaa

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -45,32 +45,10 @@ module.exports = class SkipCommand extends Command {
       errskipEmbed.setDescription(`:x: | You can't skip a trivia! Use end-trivia`)
       return message.say(errskipEmbed);
     }
-
-    if (message.guild.musicData.nowPlaying.memberDisplayName !== message.member.user.tag) {
-      let usersC = message.member.voice.channel.members.size;
-      let required = Math.ceil(usersC/2);
-      
-      if(message.guild.musicData.svote.includes(message.member.id))
-      return message.channel.send(":x: | You already voted to skip!")
-  
-	    message.guild.musicData.svote.push(message.member.id)
-
-      if(message.guild.musicData.svote.length >= required){
-        message.guild.musicData.sloop = message.guild.musicData.loop;
-        message.guild.musicData.loop = 'off';
-        message.guild.musicData.songDispatcher.end();
-        message.react('⏭️');
-        setTimeout(function onTimeOut() { message.guild.musicData.loop = message.guild.musicData.sloop }, 500);
-        return;
-      }
-
-      message.channel.send(`:white_check_mark: | You voted to skip the song \`${message.guild.musicData.svote.length}\`/\`${required}\` votes`)
-    } else {
       message.guild.musicData.sloop = message.guild.musicData.loop;
       message.guild.musicData.loop = 'off';
       message.guild.musicData.songDispatcher.end();
       message.react('⏭️');
       setTimeout(function onTimeOut() { message.guild.musicData.loop = message.guild.musicData.sloop }, 500);
-    }
   }
 };
